cbd308324bb8e220273de088dd8000fa96bf386b
Viktoras Domarkas <vidomarkas@gmail.com>
2019-02-20 18:48:23 +0000

    add js move bubbles

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let bubble = document.getElementsByClassName('reviews__bubbles__bubble')[0];
+
+setTimeout(function () {
+    bubble.style.transform = "rotate(359deg)";
+
+
+}, 1);
+
+// for media queries using js
+
+// if (window.matchMedia("(max-width: 700px)").matches) {
+//     /* The viewport is less than, or equal to, 700 pixels wide */
+//   } else {
+//     /* The viewport is greater than 700 pixels wide */
+//   }
