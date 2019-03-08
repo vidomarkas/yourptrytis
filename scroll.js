18aca8a127e243bc749170ef6a55ce6b79288363
Viktoras Domarkas <vidomarkas@gmail.com>
2019-03-08 21:49:21 +0000

    highlight nav links on scroll

--- scroll.js
+++ scroll.js
@@ -1,18 +1,40 @@
-const nav = document.getElementById("navigation");
-const services = document.getElementById("services");
-const servicesLink = document.getElementsByClassName("navigation__item")[1];
+// const nav = document.getElementById("navigation");
+const home = document.getElementById("header");
+const homeLink = document.getElementById("homeLink");
+const servicesLink = document.getElementById("servicesLink");
+// const services = document.getElementById("services");
+const reviews = document.getElementById("reviews");
+const contact = document.getElementById("contact");
 
-function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+
+window.onscroll = function () {
+    console.log("home", home.getBoundingClientRect().top);
+    console.log("services", services.getBoundingClientRect().top);
+    console.log("reviews", reviews.getBoundingClientRect().top);
+    console.log("contact", contact.getBoundingClientRect().top);
+
+
+    if (-500 < home.getBoundingClientRect().top && home.getBoundingClientRect().top < 350) {
+        homeLink.classList.add("navigation__item--selected");
+        servicesLink.classList.remove("navigation__item--selected");
+        contactLink.classList.remove("navigation__item--selected");
+        reviewsLink.classList.remove("navigation__item--selected");
+
+    } else if (-500 < services.getBoundingClientRect().top && services.getBoundingClientRect().top < 350) {
+        homeLink.classList.remove("navigation__item--selected");
+        servicesLink.classList.add("navigation__item--selected");
+        reviewsLink.classList.remove("navigation__item--selected");
+    } else if (-500 < reviews.getBoundingClientRect().top && reviews.getBoundingClientRect().top < 350) {
+
+        homeLink.classList.remove("navigation__item--selected");
+        servicesLink.classList.remove("navigation__item--selected");
+        reviewsLink.classList.add("navigation__item--selected");
+        contactLink.classList.remove("navigation__item--selected");
+    } else if (-800 < contact.getBoundingClientRect().top && contact.getBoundingClientRect().top < 350) {
+
+        homeLink.classList.remove("navigation__item--selected");
+        reviewsLink.classList.remove("navigation__item--selected");
+        contactLink.classList.add("navigation__item--selected");
+    }
 }
 
-if (isElementInViewport(navigation)) {
-    servicesLink.classList.add("navigation__item--selected");
-    console.log("added");
-} 
